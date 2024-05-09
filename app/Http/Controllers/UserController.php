b53cc37e6ff2037b6a8d122d480bd813e65f5f87
Neeraj <yneeraj082r@gmail.com>
2024-05-09 18:55:36 +0530

    Form handling completed

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -8,12 +8,28 @@ use Illuminate\Validation\Rule;
 
 class UserController extends Controller
 {
+    public function login (Request $request){
+        $incomingFields = $request->validate([
+            'loginusername'=>'required',
+            'loginpassword'=>'required'
+        ]);
+
+        if(auth()->attempt(['username'=>$incomingFields['loginusername'],'password'=>$incomingFields['loginpassword']])){
+            $request->session()->regenerate();
+            return 'You are logged in.';
+        }
+        else{
+            return 'Invalid login credentials.';
+        }
+
+
+    }
     public function register(Request $request)
     {
         $incomingFields = $request->validate([
             'username' => ['required', 'min:3', 'max:25',Rule::unique('users', 'username')],
             'email' => ['required', 'email', Rule::unique('users', 'email')],
-            'password' => ['required', 'min:8', 'max:255', 'confirmed']
+            'password' => ['required', 'min:3', 'max:255', 'confirmed']
         ]);
         User::create($incomingFields);
         return 'This is the register page.';
--- routes/web.php
+++ routes/web.php
@@ -22,3 +22,6 @@ Route::get('/', [ExampleController::class, 'homepage']);
 Route::get('/about', [ExampleController::class, 'aboutPage']);
 
 Route::post('/register', [UserController::class, 'register']);
+
+Route::post('/login', [UserController::class, 'login']);
+
